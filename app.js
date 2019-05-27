1bf8ea6f02f3bfd45ed8086033d34bfb21b48b35
TahamiTofique <tahamitofique@hotmail.com>
2019-05-27 22:54:49 +0500

    undo tsk and filter

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const val=document.querySelector('#task');
-const addbtn=document.querySelector('input#btn');
 const form=document.querySelector('form');
-const taskk=document.getElementById('task');
-form.addEventListener('submit',addtask);
+const tasklist=document.querySelector('.collection');
+const filter=document.querySelector("#filter");
+
+form.addEventListener('submit', addtask);
+tasklist.addEventListener('click', deltask);
+filter.addEventListener('keyup',filterr);
+
 function addtask(e){
     const li=document.createElement('li');
     const link=document.createElement('a');
@@ -15,6 +19,33 @@ function addtask(e){
     li.appendChild(link);
     link.appendChild(ii);
     li.appendChild(document.createTextNode(val.value));
-    taskk.value='';
+    val.value='';
     e.preventDefault();
+}
+
+function deltask(e){
+    if(e.target.parentElement.classList.contains('delete-item'))
+    {
+        
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+
+
+function filterr(e){
+    const txt=e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(
+        function(li){
+            const item=li.firstChild.textContent.toLowerCase();
+            if(item.indexOf(txt)!=-1)
+            {
+                li.style.display='block';
+            }
+            else
+            {
+                li.style.display='none';
+            }
+        }
+    );
 }
