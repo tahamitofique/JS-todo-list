ec1ca0638b0a6bebbd3804d8503f4f4de67502c9
TahamiTofique <tahamitofique@hotmail.com>
2019-05-29 06:23:54 +0500

    store and retrieve in/from localStorage

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const filter=document.querySelector("#filter");
 form.addEventListener('submit', addtask);
 tasklist.addEventListener('click', deltask);
 filter.addEventListener('keyup',filterr);
+document.addEventListener('DOMContentLoaded',loadLS);
 
 function addtask(e){
     const li=document.createElement('li');
@@ -19,8 +20,43 @@ function addtask(e){
     li.appendChild(link);
     link.appendChild(ii);
     li.appendChild(document.createTextNode(val.value));
+    storeTaskLS(val.value);
     val.value='';
     e.preventDefault();
+}
+function storeTaskLS(taskk){
+    let taskss;
+    if(localStorage.getItem('taskss')===null){
+        taskss=[];}
+    else{
+        taskss=JSON.parse(localStorage.getItem('taskss'));
+    }
+    taskss.push(taskk);
+    localStorage.setItem('taskss',JSON.stringify(taskss));
+}
+
+function loadLS(){
+    let taskss;
+    if(localStorage.getItem('taskss')===null)
+    {taskss=[];}
+    else
+    {
+        taskss=JSON.parse(localStorage.getItem('taskss'));
+    }
+    taskss.forEach( function(tas){
+        const li=document.createElement('li');
+        const link=document.createElement('a');
+        const ii=document.createElement('i');
+        li.className='collection-item';
+        link.setAttribute('href','#');
+        link.className='delete-item secondary-content';
+        ii.className='fa fa-remove';
+        document.querySelector('ul.collection').appendChild(li);
+        li.appendChild(link);
+        link.appendChild(ii);
+        li.appendChild(document.createTextNode(tas));
+    });
+
 }
 
 function deltask(e){
@@ -29,6 +65,7 @@ function deltask(e){
         
         e.target.parentElement.parentElement.remove();
     }
+    
 }
 
 
@@ -37,7 +74,7 @@ function filterr(e){
     const txt=e.target.value.toLowerCase();
     document.querySelectorAll('.collection-item').forEach(
         function(li){
-            const item=li.firstChild.textContent.toLowerCase();
+            const item=li.textContent.toLowerCase();
             if(item.indexOf(txt)!=-1)
             {
                 li.style.display='block';
